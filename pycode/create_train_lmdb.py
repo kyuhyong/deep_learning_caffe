e8d42e8c3fff7cfadab9fc751619504299131f4b
Kyubot <kyuhyong@gmail.com>
2018-01-21 23:45:07 +0900

    Initial commit

---
+++ pycode/create_train_lmdb.py
@@ -0,0 +1,97 @@
+
+# coding: utf-8
+# File name: create_train_lmdb.py
+# Author: Kyuhyong
+# Date created: 1/16/2018
+# Last modified: 1/16/2018
+# Python Version: 2.7
+# Description:
+# Creates LMDB file from following train data structure under user input directory
+# root of input/train/cat.###.jpg      label: 0
+#                    /dog.###.jpg      label: 1
+# LMDB file will be generated under user input folder
+# This code is modified from the code originally created by Adil Moujahid
+
+import os
+import sys
+import glob
+import random
+import numpy as np
+import cv2
+from caffe.proto import caffe_pb2
+import lmdb
+
+#Size of images
+IMAGE_WIDTH = 227
+IMAGE_HEIGHT = 227
+
+def transform_img(img, img_width=IMAGE_WIDTH, img_height=IMAGE_HEIGHT):
+
+    #Histogram Equalization
+    img[:, :, 0] = cv2.equalizeHist(img[:, :, 0])
+    img[:, :, 1] = cv2.equalizeHist(img[:, :, 1])
+    img[:, :, 2] = cv2.equalizeHist(img[:, :, 2])
+
+    #Image Resizing
+    img = cv2.resize(img, (img_width, img_height), interpolation = cv2.INTER_CUBIC)
+    return img
+
+def make_datum(img, label):
+    #image is numpy.ndarray format. BGR instead of RGB
+    return caffe_pb2.Datum(
+        channels=3,
+        width=IMAGE_WIDTH,
+        height=IMAGE_HEIGHT,
+        label=label,
+        data=np.rollaxis(img, 2).tostring())
+
+pathToInput = sys.argv[1]
+sys.path.insert(1, pathToInput)
+train_lmdb = pathToInput+'train_lmdb'
+validation_lmdb = pathToInput+'validation_lmdb'
+pathToTrain = pathToInput+'train'
+if not os.path.isdir(pathToTrain):
+	print "No train folder exist. Exit."
+	sys.exit(0)
+print "Generate train lmdb to {}".format(train_lmdb)
+os.system('rm -rf  ' + train_lmdb)
+in_db = lmdb.open(train_lmdb, map_size=int(1e12))
+train_data = [img for img in glob.glob(pathToTrain+"/*jpg")]
+dataNum = len(train_data)
+#Shuffle train_data
+random.shuffle(train_data)
+with in_db.begin(write=True) as in_txn:
+    for in_idx, img_path in enumerate(train_data):
+	if in_idx % 6 == 0:		# 5/6 of train data used for train
+		continue
+	img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+	img = transform_img(img, img_width=IMAGE_WIDTH, img_height=IMAGE_HEIGHT)
+	if 'cat' in img_path:
+	    label = 0
+	else:
+	    label = 1
+	datum = make_datum(img, label)
+	in_txn.put('{:0>5d}'.format(in_idx), datum.SerializeToString())
+	print '{:d} of {:d}|'.format(in_idx,dataNum) + os.path.basename(os.path.dirname(img_path)) + "/"+ os.path.basename(img_path) + ' as {:d}'.format(label),"          \r",
+in_db.close()
+print "\nTraining LMDB is created in "+train_lmdb
+
+print "Generate validation lmdb to {}".format(validation_lmdb)
+os.system('rm -rf  ' + validation_lmdb)
+in_db = lmdb.open(validation_lmdb, map_size=int(1e12))
+with in_db.begin(write=True) as in_txn:
+    for in_idx, img_path in enumerate(train_data):
+	if in_idx % 6 != 0:		# only 1/6 of train data used for validation
+		continue
+	img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+	img = transform_img(img, img_width=IMAGE_WIDTH, img_height=IMAGE_HEIGHT)
+	if 'cat' in img_path:
+	    label = 0
+	else:
+	    label = 1
+	datum = make_datum(img, label)
+	in_txn.put('{:0>5d}'.format(in_idx), datum.SerializeToString())
+	print '{:d} of {:d} @'.format(in_idx, dataNum) + os.path.basename(os.path.dirname(img_path)) + "/"+ os.path.basename(img_path) + ' as {:d}'.format(label),"             \r",
+in_db.close()
+print "\nValidation LMDB is created in " + validation_lmdb
+
---
+++ pycode/make_predictions_1.py
@@ -0,0 +1,125 @@
+'''
+Title           :make_predictions_1.py
+Description     :This script makes predictions using the 1st trained model and generates a submission file.
+Author          :Adil Moujahid
+Date Created    :20160623
+Date Modified   :20160625
+version         :0.2
+usage           :python make_predictions_1.py
+python_version  :2.7.11
+'''
+
+import os
+import sys
+import shutil
+import glob
+import cv2
+import caffe
+import lmdb
+import numpy as np
+from caffe.proto import caffe_pb2
+
+caffe.set_mode_gpu() 
+
+#Size of images
+IMAGE_WIDTH = 227
+IMAGE_HEIGHT = 227
+
+'''
+Image processing helper function
+'''
+
+def transform_img(img, img_width=IMAGE_WIDTH, img_height=IMAGE_HEIGHT):
+
+    #Histogram Equalization
+    img[:, :, 0] = cv2.equalizeHist(img[:, :, 0])
+    img[:, :, 1] = cv2.equalizeHist(img[:, :, 1])
+    img[:, :, 2] = cv2.equalizeHist(img[:, :, 2])
+
+    #Image Resizing
+    img = cv2.resize(img, (img_width, img_height), interpolation = cv2.INTER_CUBIC)
+
+    return img
+
+
+'''
+Reading mean image, caffe model and its weights 
+'''
+#Read mean image
+mean_blob = caffe_pb2.BlobProto()
+inputPath = sys.argv[1]
+sys.path.insert(1, inputPath)
+meanBinary = inputPath + 'mean.binaryproto'
+if not os.path.exists(meanBinary):
+    print "Mean binary file not exist."
+    sys.exit(0)
+with open(meanBinary) as f:
+    mean_blob.ParseFromString(f.read())
+mean_array = np.asarray(mean_blob.data, dtype=np.float32).reshape(
+    (mean_blob.channels, mean_blob.height, mean_blob.width))
+modelPath = sys.argv[2]
+sys.path.insert(1, modelPath)
+caffeProto = modelPath + 'caffenet_deploy_1.prototxt'
+if not os.path.exists(caffeProto):
+   print "Prototxt file not exist"
+   sys.exit(0)
+caffeModel = modelPath + 'caffe_model_1_iter_10000.caffemodel'
+if not os.path.exists(caffeModel):
+   print "Caffe model not exists"
+   sys.exit(0)
+#Read model architecture and trained model's weights
+net = caffe.Net(caffeProto,
+                caffeModel,
+                caffe.TEST)
+
+#Define image transformers
+transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
+transformer.set_mean('data', mean_array)
+transformer.set_transpose('data', (2,0,1))
+
+'''
+Making predicitions
+'''
+testPath = sys.argv[3]
+sys.path.insert(1, testPath)
+#Reading image paths
+test_img_paths = [img_path for img_path in glob.glob(testPath+"/*jpg")]
+
+#Making predictions
+predPath = testPath + 'predict'
+if os.path.exists(predPath):
+    os.system('rm -rf  ' + predPath)
+os.makedirs(predPath)
+dir1path = predPath + '/c1'
+dir2path = predPath + '/c2'
+os.makedirs(dir1path)
+os.makedirs(dir2path)
+test_ids = []
+preds = []
+for img_path in test_img_paths:
+    img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+    img = transform_img(img, img_width=IMAGE_WIDTH, img_height=IMAGE_HEIGHT)
+    
+    net.blobs['data'].data[...] = transformer.preprocess('data', img)
+    out = net.forward()
+    pred_probas = out['prob']
+
+    test_ids = test_ids + [img_path.split('/')[-1][:-4]]
+    preds = preds + [pred_probas.argmax()]
+    argmax = pred_probas.argmax()
+    if argmax == 1:
+        shutil.copy2(img_path, dir1path)
+    else:
+        shutil.copy2(img_path, dir2path)
+    print img_path
+    print pred_probas.argmax()
+    print '-------'
+
+'''
+Making submission file
+'''
+with open(modelPath +"submission_model_1.csv","w") as f:
+    f.write("id,label\n")
+    for i in range(len(test_ids)):
+        f.write(str(test_ids[i])+","+str(preds[i])+"\n")
+f.close()
